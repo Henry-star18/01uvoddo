fc6636d9ae370ee85b1fb9b0b4c41dc9794b0519
Henry-star18 <henry.mbuyi@student.spsezatec.cz>
2025-03-26 20:44:44 +0100

    Add files via upload

---
+++ Ůkol 1/script.js
@@ -0,0 +1,13 @@
+// Funkce, která se spustí po kliknutí na tlačítko
+<script>
+document.getElementById("Auto 1").innerHTML = "Sportovní auta mají vysoký výkon, agilita, lehká konstrukce. Příklady: Porsche 911, Ferrari F8, Corvette";
+</script>
+
+<script>
+document.getElementById("Auto 2").innerHTML = "Rodinné Suv má prostor pro 5-7 osob, pohodlné, vhodné na off-road. Příklady: Volkswagen Tiguan, Toyota RAV4, BMW X5.";
+</script>
+
+<script>
+document.getElementById("Auto 3").innerHTML = "Elektrické auto nemá žádné emise, nízké náklady, tichý chod. Příklady: Tesla Model 3, Volkswagen ID.4, Ford Mustang Mach-E.";
+</script>
+
